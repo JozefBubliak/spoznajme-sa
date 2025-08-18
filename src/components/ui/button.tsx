ddd019d200a4317ca30fbe849ccff85937493e94
JozefBubliak <jozef.bubliak@gmail.com>
2025-08-18 20:26:07 +0200

    fix(ui): add asChild support to Button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,5 +1,6 @@
 // src/components/ui/button.tsx
 import * as React from "react"
+import { Slot } from "./slot"
 import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
@@ -29,12 +30,20 @@ const buttonVariants = cva(
   }
 )
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {}
-
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
     return (
-      <button className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...(props as any)}
+      />
     )
   }
 )
