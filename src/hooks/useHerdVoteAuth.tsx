6efefcf877509154558d3e32546277ccb6576ca3
JozefBubliak <jozef.bubliak@gmail.com>
2025-08-22 21:04:30 +0200

    chore: consolidate login flow

--- src/hooks/useHerdVoteAuth.tsx
+++
@@ -1,4 +0,0 @@
-import { createUseAuth } from './createUseAuth'
-import { supabaseHerd } from '@/lib/supabaseHerdClient'
-
-export const useHerdVoteAuth = createUseAuth(supabaseHerd, 'sb-herd-auth-token')
