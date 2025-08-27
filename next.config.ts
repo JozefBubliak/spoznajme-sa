d8d7de2fbed23d2ff7114eb1641433167cc0c58e
JozefBubliak <jozef.bubliak@gmail.com>
2025-08-27 12:44:31 +0200

    Disable lint in build

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const SUP = ["en","sk","cs","pl","hu","fr","de","uk","ru","es"]
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
